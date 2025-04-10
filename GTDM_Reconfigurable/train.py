724cb58207eec71df17a9abf238ea3910b00d4bc
Jason Wu <jasonw02020@gmail.com>
2025-04-10 11:17:32 -0700

    Train.py updated with comments and cleaned up

--- GTDM_Reconfigurable/train.py
+++ GTDM_Reconfigurable/train.py
@@ -1,26 +1,23 @@
 import numpy as np
 import os
-from tqdm import tqdm, trange
+from tqdm import trange
 
 import torch
-import torch.nn as nn
 from torch.optim import Adam
-from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 from PickleDataset import PickleDataset
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 from cache_datasets import cache_data
 from models.GTDM_Model import GTDM_Early
-import argparse
 import random
 import configargparse
-from PickleDataset import transform_noise, transform_mask
+from PickleDataset import transform_noise
 
 def mseloss(t1, t2):
     sum = 0
     for i in range(len(t1)):
-        sum += (t1[i].item() - t2[i].item()) ** 2
+        sum += (t1[i] - t2[i]) ** 2 # Previously used .item() on each tensor, which decoupled from the computational graph and wasn't accounted for in loss
     return sum ** 0.5
 
 def get_args_parser():
@@ -31,7 +28,7 @@ def get_args_parser():
     parser.add("--cache_dir", type=str, help="Directory to cache datasets")
     parser.add("--valid_mods", type=str, nargs="+", help="List of valid modalities")
     parser.add("--valid_nodes", type=int, nargs="+", help="List of valid nodes")
-    parser.add("--learning_rate", type=float, default=1e-5, help="Learning rate for training")
+    parser.add("--learning_rate", type=float, default=5e-4, help="Learning rate for training")
     parser.add("--num_epochs", type=int, default=200, help="Number of epochs to train")
     parser.add("--adapter_hidden_dim", type=int, default=512, help="Dimension of adapter hidden layers")
     parser.add("--batch_size", type=int, default=32, help="Batch size for training")
@@ -102,7 +99,7 @@ def get_args_parser():
 
 
 def main(args):
-    
+    # Enable reproducability with seed
     seedVal = int(args.seedVal)
     print("Starting training with seed value", args.seedVal)
     torch.backends.cudnn.deterministic = True
@@ -110,13 +107,15 @@ def main(args):
     torch.manual_seed(seedVal)
     torch.cuda.manual_seed(seedVal)
     np.random.seed(seedVal)
+
     # Get current date and time to create new training directory within ./logs/ to store model weights
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y %H_%M_%S")
     os.mkdir('./logs/' + dt_string)
+
     cache_data(args) # Runs cacher from the data_configs.py file, will convert hdf5 to pickle if not already done
     
-    #PickleDataset inherits from a Pytorch Dataset, creates train and val datasets
+    # Create train and val datasets, these are going to be clean datasets that we then corrupt with noise through transform_noise
     trainset = PickleDataset(args.cache_dir + 'train', args.valid_mods, args.valid_nodes)
     valset = PickleDataset(args.cache_dir + 'val', args.valid_mods, args.valid_nodes)
     batch_size = args.batch_size
@@ -131,9 +130,10 @@ def main(args):
     
 
     # Create the overall model and load on appropriate device
+    # IN GTDM Early, we initialize with the MAE pretrained weights if we pass in 12 layers for image and depth and then freeze the majority of the layers
     model = GTDM_Early(args.adapter_hidden_dim, valid_mods=args.valid_mods, valid_nodes = args.valid_nodes, layerdrop=0.0, vision_vit_layers=args.vision_vit_layers, depth_vit_layers=args.depth_vit_layers)
     model.to(device)
-    #model.load_state_dict(torch.load('./MAE_Dropout_FT_Dropout.pth'), strict=False)
+    
 
     #Establish from training parameters
 
@@ -142,7 +142,6 @@ def main(args):
    
    
     # Training loop
-
     for epoch in trange(args.num_epochs, desc="Training"):
         batch_num = 0
         epoch_train_loss = 0
@@ -161,24 +160,29 @@ def main(args):
             data, gt_pos = batch['data'], batch['gt_pos']
             # Data itself is a dictionary with keys ('modality', 'node') that points to data of dimension batch_size
             gt_pos = gt_pos.to(device)
+
+            # Do not add noise to the first 5 epochs
             if epoch < 5:
                 data, _ = transform_noise(data, args.batch_size, img_std_max = 0, depth_std_max = 0)
             else:
                 data, _ = transform_noise(data, args.batch_size, img_std_max=3, depth_std_max=0.75)
+
             # Perform forward pass
             batch_results = model(data) #Dictionary
             # key is still ('modality', 'node') with a distribution estimated by the model
             for key in batch_results.keys():
                 for i in range(len(batch_results[key]['dist'])):
-                    # TODO Currently 2D, also introduce hybrid training, use MSE to help convergence at start then use NLL
+                    # Use both mse and nll to supervise training
                     loss_mse = mseloss(torch.squeeze(batch_results[key]['dist'][i].mean), torch.squeeze(gt_pos[i][:, [0, 2]]))
-                    pos_neg_log_probs =  -batch_results[key]['dist'][i].log_prob(torch.squeeze(gt_pos[i][:, [0, 2]])) # Computes NLL loss for each node/modality combo
+                    pos_neg_log_probs = -batch_results[key]['dist'][i].log_prob(torch.squeeze(gt_pos[i][:, [0, 2]])) # Computes NLL loss for each node/modality combo
                     train_loss += pos_neg_log_probs + 0.05 * loss_mse # Accumulate all the losses into the batch loss
-                    ad_train_loss += loss_mse / (batch_size * len(batch_results.keys()))
+                    with torch.no_grad(): # Get the average distance during train to log
+                        ad_train_loss += loss_mse 
             train_loss /= (batch_size * len(batch_results.keys())) # Normalize wrt batch size and number of modality node combinations
-           
-
+            
             with torch.no_grad():
+                # No need to track grads for ad train loss
+                ad_train_loss /= (batch_size * len(batch_results.keys()))
                 # Print one sample from the batch to see prediction result and loss
                 print('Batch Number', batch_num)
                 key = 'early_fusion'
@@ -186,9 +190,9 @@ def main(args):
                 sample_mse_loss =  mseloss(torch.squeeze(batch_results[key]['dist'][0].mean), torch.squeeze(gt_pos[0][:, [0, 2]]))
                 sample_nll_loss =  -batch_results[key]['dist'][0].log_prob(torch.squeeze(gt_pos[0][:, [0, 2]]))
                 print('\tGT', gt_pos[0], 'with loss', sample_nll_loss + 0.05 * sample_mse_loss)
-                #print('\tGT', gt_pos[0], 'with loss', mseloss(batch_results['img', 'node_1']['dist'][0].mean, gt_pos[0][:, 0:2]))
                 print('-------------------------------------------------------------------------------------------------------------------------------')
                 epoch_train_loss += train_loss # Accumulate batch loss into overall epoch loss
+
             # Backprop and update
             train_loss.backward()
             optimizer.step() 
@@ -197,6 +201,7 @@ def main(args):
         ad_train_loss /= batch_num
         writer.add_scalar("Training loss", epoch_train_loss / batch_num, epoch)
 
+        # Peform validation
         batch_num = 0
         epoch_val_loss = 0
         with torch.no_grad():
@@ -206,8 +211,8 @@ def main(args):
                 val_loss = 0.0
                 # Each batch is a dictionary containing all the sensor data, and the ground truth positions
                 data, gt_pos = batch['data'], batch['gt_pos']
-                # Data itself is a dictionary with keys ('modality', 'node') that points to data of dimension batch_size
                 gt_pos = gt_pos.to(device)
+                # Get the noise
                 data, _ = transform_noise(data, args.batch_size, img_std_max=3, depth_std_max=0.75)
                 
                 # Perform forward pass
@@ -215,10 +220,7 @@ def main(args):
                 # key is still ('modality', 'node') with a distribution estimated by the model
                 for key in batch_results.keys():
                     for i in range(len(batch_results[key]['dist'])):     
-
-                        loss_mse = mseloss(torch.squeeze(batch_results[key]['dist'][i].mean), torch.squeeze(gt_pos[i][:, [0, 2]]))
-                        pos_neg_log_probs =  -batch_results[key]['dist'][i].log_prob(torch.squeeze(gt_pos[i][:, [0, 2]])) # Computes NLL loss for each node/modality combo
-                        val_loss += loss_mse # Accumulate all the losses into the batch loss
+                        val_loss += mseloss(torch.squeeze(batch_results[key]['dist'][i].mean), torch.squeeze(gt_pos[i][:, [0, 2]]))
                 val_loss /= (len(batch_results[key]['dist']) * len(batch_results.keys())) # Normalize wrt batch size and number of modality node combinations
                 epoch_val_loss += val_loss
             epoch_val_loss /= batch_num
